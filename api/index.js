4b860ed8d6318342ad9e536da0fcdbd209cdf8b9
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-07-31 16:33:43 -0400

    finished login and register page

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const User = require('./models/User');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 dotenv.config();
 
@@ -24,25 +24,30 @@ async function connectToDatabase() {
 connectToDatabase();
 
 const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
+// Development settings for cookies
+const cookieSettings = {
+  sameSite: 'none', // Use 'Lax' or 'Strict' in production
+  secure: false,    // Use 'true' in production (over HTTPS)
+};
+
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
 }));
-
-
 
 app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
 app.get('/profile', (req, res) => {
-    const { token } = req.cookies?.token;
+    const { token } = req.cookies || {};
     if (token) {
         jwt.verify(token, jwtSecret, {}, (err, userData) => {
             if (err) throw err;
@@ -55,18 +60,35 @@ app.get('/profile', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const doundUser = await User.findOne({ username });
-
-
+    const foundUser = await User.findOne({ username });
+    if (foundUser) {
+        const passOk = bcrypt.compareSync(password, foundUser.password);
+        if (passOk) {
+            jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token, cookieSettings).json({
+                    id: foundUser._id,
+                });
+            });
+        } else {
+            res.status(401).json('incorrect password');
+        }
+    } else {
+        res.status(404).json('user not found');
+    }
 });
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
-        const createdUser = await User.create({ username, password });
+        const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
+        const createdUser = await User.create({
+            username: username,
+            password: hashedPassword
+        });
         jwt.sign({ userID: createdUser._id, username }, jwtSecret, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('token', token, { sameSite: 'none', secure: true }).status(201).json({ id: createdUser._id, });
+            res.cookie('token', token, cookieSettings).status(201).json({ id: createdUser._id });
         });
     } catch (err) {
         console.error(err);
@@ -74,6 +96,7 @@ app.post('/register', async (req, res) => {
     }
 });
 
-app.listen(4040, () => {
-    console.log('Server is running on port 4040');
+const port = process.env.PORT || 4040;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
