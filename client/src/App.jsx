c26019892489f0c95271b37a86b90c9e18a59bbf
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-07-31 23:38:49 -0400

    finished project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-import axios from "axios"
-import { UserContextProvider } from "./UserContext";
+import axios from "axios";
+import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
   axios.defaults.baseURL = 'http://localhost:4040';
   axios.defaults.withCredentials = true;
-  
   return (
     <UserContextProvider>
       <Routes />
